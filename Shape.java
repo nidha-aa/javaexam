5cadf2a5b84d722ed49c45d57120a9b2ce003dae
nidha-aa <84379800+nidha-aa@users.noreply.github.com>
2021-10-06 16:22:13 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,54 @@
+import java.util.*;
+class perimeter
+
+{
+
+public void perimeter(return)
+
+}
+{
+{
+class rectangle implements perimeter
+{
+float l;
+float b;
+return(2*(l+b))
+}
+
+
+class square implements perimeter
+{
+float a;
+return(4*a)
+}
+
+class circle implements perimeter
+{
+float pi=3.14;
+int r;
+return( 2 * pi * r ) 
+}
+
+class Shape implements perimeter
+{
+public static void main(String args[])
+{
+int rec;
+int s;
+int c;
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the length");
+l=nextInt.sc;
+System.out.println("Enter the breadth");
+b=nextInt.sc;
+System.out.println(("The perimeter of rectangle is:")+rec("sq.meter"));
+System.out.println("Enter the side");
+a=nextInt.sc;
+System.out.println(("The perimeter of squarer is:")+s("sq.meter"));
+System.out.println("Enter the radius of the circle");
+r=nextInt.sc;
+System.out.println(("The perimeter of the circle is:")+c("sq.meter"));
+}
+}
+}
+
---
+++ sumeven.java
@@ -0,0 +1,19 @@
+class sumeven
+{
+public static void main(String args[])
+{
+int num;
+int i;
+
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter a number");
+num=sc.nextInt;
+for(i=0;i<num;i++)
+{
+if(i%2==0)
+System.out.println("the even numbers");
+}
+
+System.out.println("The sum of even numbers");
+}
+}
